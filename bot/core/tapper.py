c836f3af6eb5ab64a3c82b95e2d95820613fb175
Vanhbaka <vanhbaka2k7@gmail.com>
2024-09-29 10:04:06 +0700

    Update tapper.py

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -458,7 +458,7 @@ async def run_tapper(tg_client: Client, proxy: str | None):
     try:
         sleep_ = randint(1, 15)
         logger.info(f"{tg_client.name} | start after {sleep_}s")
-        # await asyncio.sleep(sleep_)
+        await asyncio.sleep(sleep_)
         await Tapper(tg_client=tg_client).run(proxy=proxy)
     except InvalidSession:
         logger.error(f"{tg_client.name} | Invalid Session")
